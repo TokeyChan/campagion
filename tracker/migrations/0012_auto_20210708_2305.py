77d58d547619b644fc4f76109d5ab124e049cfaf
TokeyChan <bengames@icloud.com>
2021-09-07 21:53:36 +0200

    migrations fix

--- tracker/migrations/0012_auto_20210708_2305.py
+++ tracker/migrations/0012_auto_20210708_2305.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='node',
             name='task',
-            field=models.OneToOneField(default=0, on_delete=django.db.models.deletion.CASCADE, to='tracker.task'),
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='tracker.task'),
             preserve_default=False,
         ),
     ]
