e82b358e5161a6ba736d514eadf385fb4f4de69c
TokeyChan <bengames@icloud.com>
2021-09-07 21:37:22 +0200

    database settings

--- campagion/settings.py
+++ campagion/settings.py
@@ -85,12 +85,24 @@ WSGI_APPLICATION = 'campagion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if config('SQLITE', cast=bool):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': config('DATABASE_ENGINE', cast=str),
+            'NAME': config('DATABASE_NAME', cast=str),
+            'USER': config('DATABASE_USER', cast=str),
+            'PASSWORD': config('DATABASE_PASSWORD', cast=str),
+            'HOST': config('DATABASE_HOST', cast=str),
+            'PORT': config('DATABASE_PORT', cast=int)
+        }
+    }
 
 
 # Password validation
